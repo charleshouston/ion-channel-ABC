9385ceaaf3c02f6ec5293b0cfa601527d6ac9f0a
Charles Houston <charles.houston8@gmail.com>
2017-06-10 14:23:34 +0100

    Finding results files within results folder.

--- display_results.py
+++ display_results.py
@@ -30,7 +30,7 @@ s = myokit.Simulation(m)
 res_orig = channel.simulate(s)
 
 # Open results from ABC simulation
-f = open('results_' + str(channel.name) + '.txt')
+f = open('results/results_' + str(channel.name) + '.txt')
 pool = f.readline()
 pool = ast.literal_eval(pool)
 
