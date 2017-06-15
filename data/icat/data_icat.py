d8bfc767ef6fc6895cf326e3e960402aede999eb
Charles Houston <charles.houston11@imperial.ac.uk>
2017-06-15 15:07:39 +0100

    Sensible naming for iCaT data.

--- data/icat/data_icat.py
+++ data/icat/data_icat.py
@@ -16,7 +16,7 @@
 
     Returns x, y data from points in figure 1B
 '''
-def fig1B():
+def IV_DengFig1B():
     x = [-80,-70,-60,-50,-40,-30,-20,-10,0,10,20,30]
     y = [-0.106837,-0.492639,-2.350421,-5.561476,-11.289143,-17.396676,-20.109156,-18.643111,-14.826645,-9.751873,-5.341866,-1.697428]
     return x,y
@@ -26,7 +26,7 @@ Activation curve
 
 Returns x,y data from activation points in figure 3B
 '''
-def fig3Bact():
+def Act_DengFig3B():
     x = [-80,-70,-60,-50,-40,-30,-20,-10]
     y = [0.001373,0.027600,0.003571,0.054514,0.173841,0.497494,0.846686,1.000619]
     return x,y
@@ -36,7 +36,7 @@ Inactivation curve
 
 Returns x,y data from inactivation points in figure 3B
 '''
-def fig3Binact():
+def Inact_DengFig3B():
     x = [-100,-90,-80,-70,-60,-50,-40]
     y = [1.000205,0.990302,0.992363,0.980386,0.914755,0.534569,0.298036]
     return x,y
@@ -46,7 +46,7 @@ Recovery curve
 
 Returns x,y data from recovery points in figure 4B
 '''
-def fig4B():
+def Recovery_DengFig4B():
     x = [0.3,32.1,64.5,96.3,128.3,160.1,192.5,224.3,256.3,288.4,320.7]
     y = [0.0,0.240493,0.443266,0.577305,0.656860,0.771620,0.815969,0.894686,0.933167,0.970810,0.978277]
     return x,y
