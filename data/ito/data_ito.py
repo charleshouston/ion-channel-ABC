67e576ac397c8bacc29245c76024f51e587beba5
Charles Houston <charles.houston11@imperial.ac.uk>
2017-07-03 16:45:00 +0100

    Correcting naming of ito data figure.

--- data/ito/data_ito.py
+++ data/ito/data_ito.py
@@ -15,7 +15,7 @@ I-V curve
 
 Returns x, y data from points in Figure 6 from Kao et al., 2016.
 '''
-def IV_Kao6():
+def IV_KaoFig6():
     x = [-40,-30,-20,-10,0,10,20,30,40,50,60]
     y = [0.056990,0.296790,0.609235,0.812738,1.107084,1.727961,2.312640,3.024312,3.863076,4.592847,5.431610]
     return x, y
