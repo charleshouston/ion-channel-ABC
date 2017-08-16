bb547c337d9f5e364375871da177781e5d34d5da
Charles Houston <charles.houston11@imperial.ac.uk>
2017-08-16 15:15:45 +0100

    plot voltage clamp fit and write ABC posterior for each param to file

--- process_results.py
+++ process_results.py
@@ -17,7 +17,7 @@ from ion_channel_ABC import LossFunction
 round_to_n = lambda x, n: round(x, -int(math.floor(math.log10(abs(x)))) + (n - 1)) if x != 0 else 0
 
 # Import channel
-channel = channel_setup.icat()
+channel = channel_setup.ikur()
 m,_,_ = myokit.load('models/'+channel.model_name)
 
 # Check whether to store sim results
@@ -32,20 +32,17 @@ with open('results/results_' + channel.name + '.txt') as f:
     pool = ast.literal_eval(pool)
     weights = f.readline()
     weights = ast.literal_eval(weights)
-    if channel.name == 'ina':
-        remove = [10,57,78,92,150,186,218,220,253,279,323,380]
-        pool = [i for j,i in enumerate(pool) if j not in remove]
-        weights = [i for j,i in enumerate(weights) if j not in remove]
 
+channel.setup_simulations(continuous=True)
 if not overwrite:
     with open(plot_data_name, 'rb') as f:
         loaded_data = pickle.load(f)
         sim_original, sim_ABC = loaded_data[0], loaded_data[1]
 else:
     # Generate result with original parameters
-    channel.setup_simulations()
-    sim_original = channel.simulate()
     params_orig = [m.get(channel.name+'.'+p).value() for p in channel.parameter_names]
+    channel.reset_params(params_orig)
+    sim_original = channel.simulate()
 
     # Fill with simulations from final distribution pool
     sim_ABC = [[] for i in range(len(pool))]
@@ -56,6 +53,7 @@ else:
         channel.reset_params(params)
         sim_vals = channel.simulate()
         if sim_vals is not None:
+            sim_vals = np.array(sim_vals)
             sim_ABC[i] = sim_vals
 
     # Save plotting data to avoid further simulations
@@ -63,6 +61,9 @@ else:
         pickle.dump([sim_original, sim_ABC], f)
 
 # Calculate summary statistics
+weights = [w for i,w in enumerate(weights) if len(sim_ABC[i]) != 0]
+sim_ABC = [s for s in sim_ABC if len(s) != 0]
+
 sim_ABC = np.array(sim_ABC)
 sim_ABC_reshape = sim_ABC[:,:,1].swapaxes(0,1)
 sim_ABC_mu = []
@@ -80,10 +81,8 @@ for exper in sim_ABC_reshape:
 
 # Plot the results
 plt.style.use('seaborn-colorblind')
-
 ncols = len(sim_ABC_reshape)
 fig, ax = plt.subplots(nrows=1, ncols=ncols, figsize=(3*ncols, 2.8))
-
 for i in range(ncols):
     if ncols > 1:
         axi = ax[i]
@@ -116,48 +115,6 @@ if ncols > 1:
 plt.tight_layout()
 fig.savefig('results/fig_'+str(channel.name)+'.pdf', bbox_inches="tight")
 plt.close(fig)
-
-# Colormap using seaborn-colorblind first colour with transparency
-# cmap = np.array([[0.0, 114/256.0, 178/256.0, i] for i in np.linspace(0, 1, 256)])
-# cmap = ListedColormap(cmap)
-
-# for i in range(len(sim_ABC_params)):
-#     if len(sim_ABC) > 1:
-#         ax1 = ax[i][0]
-#         ax2 = ax[i][1]
-#     else:
-#         ax1 = ax[0]
-#         ax2 = ax[1]
-
-#     for j in range(len(sim_ABC_params[i])):
-#         normalised_weight = (weights[j]-min(weights))/(max(weights) - min(weights))
-#         ax2.plot(sim_ABC_params[i][j][0], sim_ABC_params[i][j][1], linestyle=':', c=cmap(normalised_weight), lw=0.5)
-
-#     ax1.plot(channel.simulations_x[i], sim_ABC_mu[i], '-', label='ABC simulations')
-#     ax1.fill_between(channel.simulations_x[i], sim_ABC_mu[i]-sim_ABC_sd[i], sim_ABC_mu[i]+sim_ABC_sd[i], alpha=0.25, lw=0)
-
-    # Uncomment below for median and IQR
-    # ax1.plot(x_cont2, sim_ABC_med[i], '-', label='ABC simulations')
-    # ax1.fill_between(x_cont2, sim_ABC_med[i]-sim_ABC_iqr[i], sim_ABC_med[i]+sim_ABC_iqr[i], alpha=0.25, lw=0)
-
-    # ax1.plot(channel.data_exp[i][0], channel.data_exp[i][1], 'o', label='Experimental data')
-    # ax1.plot(sim_original[i][0], sim_original[i][1], '--', label=channel.publication)
-    # ax1.set_xlabel(channel.setup_exp[i]['xlabel'])
-    # ax2.set_xlabel(channel.setup_exp[i]['xlabel'])
-    # ax1.set_ylabel(channel.setup_exp[i]['ylabel'])
-
-# if len(sim_ABC) > 1:
-#     ax[int(len(sim_ABC_params))-1][0].legend(loc='best')
-# else:
-#     ax[0].legend(loc='best')
-
-# uppercase_letters = map(chr, range(65,91))
-# for i,a in enumerate(ax.flatten()):
-#     a.text(-0.01, -0.08,
-#            uppercase_letters[i], transform=a.transAxes,
-#            fontsize=16, fontweight='bold', va='top', ha='right')
-# plt.tight_layout()
-# fig.savefig('results/fig_'+str(channel.name)+'.pdf', bbox_inches="tight")
 
 # Save summary statistics
 #  Get original parameters
