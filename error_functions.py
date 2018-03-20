b8c0731b2bc19c792239e014be0359ab212b68a5
Charles Houston <charles.houston@protonmail.com>
2018-03-20 10:56:15 +0000

    Error CVchisq defaults to CVrmsd if no N number in the data.

--- error_functions.py
+++ error_functions.py
@@ -30,39 +30,42 @@ def cvchisq(sim_results, experiment_data):
     """
     warnings.filterwarnings('error')
 
-    s = np.array(sim_results)
-    e = np.array(experiment_data.y)
-    N = np.array(experiment_data.N)
-    err_bars = np.array(experiment_data.errs)
-
-    if experiment_data.err_type == 'SEM':
-        sd = err_bars * np.sqrt(N)
-    elif experiment_data.err_type == 'STD':
-        sd = err_bars
+    if experiment_data.N is None:
+        return cvrmsd(sim_results, experiment_data)
     else:
-        raise ValueError('Need `err_type` to compute cvchisq loss!')
+        s = np.array(sim_results)
+        e = np.array(experiment_data.y)
+        N = np.array(experiment_data.N)
+        err_bars = np.array(experiment_data.errs)
 
-    # Set SD values to minimum rather than 0 to avoid divide by zero.
-    for i, sdi in enumerate(sd):
-        if sdi == 0:
-            sd[i] = np.min(sd[sd!=0])
+        if experiment_data.err_type == 'SEM':
+            sd = err_bars * np.sqrt(N)
+        elif experiment_data.err_type == 'STD':
+            sd = err_bars
+        else:
+            raise ValueError('Need `err_type` to compute cvchisq loss!')
 
-    # Normalise weights for WSS loss so that limit as SD -> 0
-    # is equivalent to sum of squares loss.
-    w = [1 / np.square(sdi) for sdi in sd]
-    w = w / np.max(w)
+        # Set SD values to minimum rather than 0 to avoid divide by zero.
+        for i, sdi in enumerate(sd):
+            if sdi == 0:
+                sd[i] = np.min(sd[sd!=0])
 
-    try:
-        err = np.sum(w * np.square(e - s))
-    except Warning:
-        return float("inf")
-    except:
-        return float("inf")
+        # Normalise weights for WSS loss so that limit as SD -> 0
+        # is equivalent to sum of squares loss.
+        w = [1 / np.square(sdi) for sdi in sd]
+        w = w / np.max(w)
 
-    err = pow(err / len(s), 0.5)
-    err = err / np.ptp(e)
-    warnings.resetwarnings()
-    return err
+        try:
+            err = np.sum(w * np.square(e - s))
+        except Warning:
+            return float("inf")
+        except:
+            return float("inf")
+
+        err = pow(err / len(s), 0.5)
+        err = err / np.ptp(e)
+        warnings.resetwarnings()
+        return err
 
 
 def cvrmsd(sim_results, experiment_data):
