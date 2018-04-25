1c350871ebfa93abf995b4cc9b9408df854d0309
Charles Houston <charles.houston@protonmail.com>
2018-04-25 21:35:27 +0100

    Passing arguments to scripts from pyabc notebook

--- get_measurements.py
+++ get_measurements.py
@@ -1,7 +1,38 @@
 import pandas as pd
+import argparse
 
-import channels.icat
-ch = channels.icat.icat
+parser = argparse.ArgumentParser()
+parser.add_argument('channel', type=str)
+args = parser.parse_args()
+
+# Import correct channel.
+ch = None
+if args.channel == 'icat':
+    import channels.icat
+    ch = channels.icat.icat
+elif args.channel == 'ikur':
+    import channels.ikur
+    ch = channels.ikur.ikur
+elif args.channel == 'ikr':
+    import channels.ikr
+    ch = channels.ikr.ikr
+elif args.channel == 'iha':
+    import channels.iha
+    ch = channels.iha.iha
+elif args.channel == 'ina':
+    import channels.ina
+    ch = channels.ina.ina
+elif args.channel == 'ito':
+    import channels.ito
+    ch = channels.ito.ito
+elif args.channel == 'ik1':
+    import channels.ik1
+    ch = channels.ik1.ik1
+elif args.channel == 'ical':
+    import channels.ical
+    ch = channels.ical.ical
+else:
+    raise ValueError("Unrecognised channel.")
 
 measurements = pd.DataFrame(columns = ['exp', 'x', 'y'])
 
--- run_channel.py
+++ run_channel.py
@@ -1,15 +1,46 @@
 import pandas as pd
+import argparse
 
-import channels.icat
-ch = channels.icat.icat
+parser = argparse.ArgumentParser()
+parser.add_argument('channel', type=str)
+args = parser.parse_known_args()
+
+# Import correct channel.
+ch = None
+if args[0].channel == 'icat':
+    import channels.icat
+    ch = channels.icat.icat
+elif args[0].channel == 'ikur':
+    import channels.ikur
+    ch = channels.ikur.ikur
+elif args[0].channel == 'ikr':
+    import channels.ikr
+    ch = channels.ikr.ikr
+elif args[0].channel == 'iha':
+    import channels.iha
+    ch = channels.iha.iha
+elif args[0].channel == 'ina':
+    import channels.ina
+    ch = channels.ina.ina
+elif args[0].channel == 'ito':
+    import channels.ito
+    ch = channels.ito.ito
+elif args[0].channel == 'ik1':
+    import channels.ik1
+    ch = channels.ik1.ik1
+elif args[0].channel == 'ical':
+    import channels.ical
+    ch = channels.ical.ical
+else:
+    raise ValueError("Unrecognised channel.")
 
 # Collect parameters from input arguments.
-import argparse
-parser = argparse.ArgumentParser()
 for p in ch.pars:
     parser.add_argument("-" + p, type=float)
 args = parser.parse_args()
-sim = ch(vars(args))
+args_d = vars(args)
+del args_d['channel']
+sim = ch(args_d)
 
 with pd.option_context('display.max_rows', -1, 'display.max_columns', 5):
     print sim.to_string()
