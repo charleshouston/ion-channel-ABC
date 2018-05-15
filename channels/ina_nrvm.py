ebe052cd6fae7871e9c35b009e2bb7ff55f063b9
Charles Houston <charles.houston@protonmail.com>
2018-05-15 14:58:55 +0100

    updating korhonen ina model

--- channels/ina_nrvm.py
+++ channels/ina_nrvm.py
@@ -6,37 +6,43 @@ import data.ina.data_ina as data
 import numpy as np
 
 
-modelfile = 'models/BeelerReuter1976_iNa.mmt'
+modelfile = 'models/Korhonen2009_iNa.mmt'
 
-parameters = dict(k1=(0, 100),
-                  k2=(-1, 0),
-                  k3=(0, 100),
-                  k4=(0, 100),
-                  k5=(-1, 0),
-                  k6=(0, 100),
-                  k7=(0, 1),
-                  k8=(-1, 0),
-                  k9=(0, 100),
-                  k10=(0, 10),
-                  k11=(-1, 0),
-                  k12=(0, 100),
-                  k13=(0, 1),
-                  k14=(-1, 0),
-                  k15=(0, 100),
-                  k16=(-1, 0),
-                  k17=(0, 1),
-                  k18=(-1, 0),
-                  k19=(0, 100))
-
-#parameters = dict(p1=(0, 100),
-#                  p2=(-10, 0),
-#                  p3=(0, 1),
-#                  p4=(0, 100),
-#                  p5=(-1, 0),
-#                  p6=(0, 1),
-#                  p7=(0, 100),
-#                  q1=(0, 100),
-#                  q2=(0, 10))
+parameters = dict(g_Na=(0, 100),
+                  p1=(0, 100),
+                  p2=(-100, 0),
+                  p3=(0, 1),
+                  p4=(0, 100),
+                  p5=(-100, 0), # Expanded from (-10, 0) after test run
+                  p6=(0, 1),
+                  p7=(0, 1000), # Expanded from (0, 500) after test run
+                  q1=(0, 100),
+                  q2=(0, 50),
+                  q3=(0, 10),
+                  q4=(-50, 50),
+                  q5=(-50, 50),
+                  q6=(0, 1),
+                  q7=(0, 100),
+                  q8=(-50, 50),
+                  q9=(0, 10),
+                  q10=(0, 1),
+                  q11=(0, 10),
+                  r1=(0, 100),
+                  r2=(-1, 0),
+                  r3=(-50, 50),
+                  r4=(-10, 0),
+                  r5=(0, 10),
+                  r6=(0, 100),
+                  r7=(0, 1),
+                  r8=(0, 100),
+                  r9=(-10, 0),
+                  r10=(0, 1),
+                  r11=(0, 10),
+                  r12=(-0.1, 0),
+                  r13=(0, 1),
+                  r14=(-1, 0),
+                  r15=(-1, 0),
+                  r16=(0, 100))
 
 ina = Channel('ina', modelfile, parameters,
               vvar='membrane.V', logvars=['ina.i_Na', 'ina.G_Na'])
@@ -48,13 +54,17 @@ stim_times = [5000, 20]
 stim_levels = [-80, iv_vsteps]
 def peak_curr(data):
     return max(data[0]['ina.i_Na'], key=abs)
+def normalise_by_peak(sim_results):
+    peak = 445.1308 # peak current determined from unmodified sim
+    sim_results = [r / peak for r in sim_results]
+    return sim_results
 def normalise(sim_results):
     m = abs(max(sim_results, key=abs))
     sim_results = [r / m for r in sim_results]
     return sim_results
 iv_prot = ExperimentStimProtocol(stim_times, stim_levels,
                                  measure_index=1, measure_fn=peak_curr,
-                                 post_fn=normalise)
+                                 post_fn=normalise_by_peak)
 conditions = dict(Nao=20000, Nai=10000, T=295)
 iv_exp = Experiment(iv_prot, iv_data, conditions)
 ina.add_experiment(iv_exp)
@@ -97,7 +107,6 @@ def normalise_curr(data):
         sim_results.append(max(record['ina.i_Na'], key=abs))
     m = max(sim_results, key=abs)
     sim_results = [r / m for r in sim_results]
-    print(sim_results)
     return sim_results
 rec_prot1 = ExperimentStimProtocol(stim_times, stim_levels,
                                    measure_index=range(1, 40, 2),
