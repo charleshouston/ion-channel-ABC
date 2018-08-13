b651dcabb5b164da6526e98aabde57b8964a7f69
Charles Houston <charles.houston@protonmail.com>
2018-08-13 16:46:54 +0100

    Updating full cell model

--- channels/hl1.py
+++ channels/hl1.py
@@ -102,7 +102,7 @@ params = {'icat.g_CaT': (0, 2),
           'ik1.k_3': (0, 1),
           'ik1.k_4': (0, 0.1),
           'icab.g_Cab': (0, 0.001),
-          'incx.k_NCX': (0, 2.6e16),
+          'incx.k_NCX': (0, 10),
           'inab.g_Nab': (0, 0.0026),
           'inak.i_NaK_max': (0, 2.7)}
 
@@ -127,10 +127,22 @@ def interpolate_align(data_list):
     for var in data:
         data_output[var] = np.interp(time, simtime, data[var])
     return data_output
+
+def resting_behaviour(data_list):
+    data = data_list[0]
+    if len(data_list) > 1:
+        for log in data_list[1:]:
+            data = data.extend(log)
+    v_rest = data['membrane.V'][-1]
+#    ca_rest = data['ca_conc.Ca_i'][-1]
+#    na_rest = data['na_conc.Na_i'][-1]
+#    k_rest = data['k_conc.K_i'][-1]
+    return [v_rest]#, ca_rest, na_rest, k_rest]
+
 resting_prot = ExperimentStimProtocol(stim_times, stim_levels,
                                       ind_var=time,
-                                      measure_index=range(len(stim_times)),
-                                      measure_fn=interpolate_align)
+                                      measure_index=len(stim_times)-1,
+                                      measure_fn=resting_behaviour)
 dias_conditions = dict(T=305,
                        Ca_o=1800,
                        Na_o=1.4e5,
@@ -139,9 +151,9 @@ resting_exp = Experiment(resting_prot, None, dias_conditions)
 hl1.add_experiment(resting_exp)
 
 n_pulses = 100
-stim_times = [10000] + [2, 998]*n_pulses + [2, 998]
+stim_times = [100000] + [2, 998]*n_pulses + [2, 998]
 stim_levels = [0] + [40, 0]*n_pulses + [40, 0]
-pace_time = np.linspace(0, 1998, 1998)
+pace_time = np.linspace(0, 5000, 5000)
 def interpolate_align_pace(data_list):
     import numpy as np
     data = data_list[0]
@@ -157,12 +169,48 @@ def interpolate_align_pace(data_list):
         data_output[var] = np.interp(pace_time, simtime, data[var])
     return data_output
 
+def ap_measurements(data_list):
+    import numpy as np
+    data = data_list[0]
+    if len(data_list) > 1:
+        for log in data_list[1:]:
+            data = data.extend(log)
+
+    # Voltage measurements
+    Vrp = data['membrane.V'][0]
+    APA = max(data['membrane.V']) - Vrp
+    V_diff = np.diff(data['membrane.V'])
+    peak_index = np.nonzero(np.array(data['membrane.V']) ==
+                            max(data['membrane.V']))[0][0]
+    APD90 = 0.0
+    for i in range(peak_index, len(data['membrane.V'])):
+        if data['membrane.V'][i] <= (Vrp + 0.1 * APA):
+            APD90 = data['environment.time'][i] - data['environment.time'][0]
+            break
+
+    # Calcium measurements
+    ca_peak_index = np.nonzero(np.array(data['ca_conc.Ca_i']) ==
+                               max(data['ca_conc.Ca_i']))[0][0]
+    ca_rp = data['ca_conc.Ca_i'][0]
+    ca_amp = max(data['ca_conc.Ca_i']) - ca_rp
+    t_ca_rise = (data['environment.time'][ca_peak_index] -
+                data['environment.time'][0])
+    t_ca50 = 0.0
+    t_ca90 = 0.0
+    for i in range(ca_peak_index, len(data['ca_conc.Ca_i'])):
+        if t_ca50 == 0.0 and (data['ca_conc.Ca_i'][i] <= (ca_rp + 0.5 * ca_amp)):
+            t_ca50 = data['environment.time'][i] - data['environment.time'][0]
+        if data['ca_conc.Ca_i'][i] <= (ca_rp + 0.1 * ca_amp):
+            t_ca90 = data['environment.time'][i] - data['environment.time'][0]
+            break
+
+    data_output = [APA, APD90, t_ca_rise, t_ca50, t_ca90]
+    return data_output
+
 pulse_train_prot = ExperimentStimProtocol(stim_times, stim_levels,
-                                          ind_var=pace_time,
-                                          measure_index=[len(stim_times)-3,
-                                                         len(stim_times)-2,
-                                                         len(stim_times)-1],
-                                          measure_fn=interpolate_align_pace)
+                                          ind_var=['apa','apd90','t_ca_rise','t_ca50','t_ca90'],
+                                          measure_index=range(len(stim_times)-2, len(stim_times)),
+                                          measure_fn=ap_measurements)
 pulse_train_exp = Experiment(pulse_train_prot, None, dias_conditions)
 hl1.add_experiment(pulse_train_exp)
 
@@ -180,4 +228,4 @@ increasing_pulse_train_prot = ExperimentStimProtocol(stim_times, stim_levels,
                                                      measure_index = range(1, len(stim_times)),
                                                      measure_fn=interpolate_align_pace)
 increasing_pulse_train_exp = Experiment(increasing_pulse_train_prot, None, dias_conditions)
-hl1.add_experiment(increasing_pulse_train_exp)
+#hl1.add_experiment(increasing_pulse_train_exp)
