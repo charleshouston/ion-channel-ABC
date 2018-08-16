8f0cc8ba6748f95be39f020c266b6c17abe1149c
Charles Houston <charles.houston@protonmail.com>
2018-08-16 15:56:18 +0100

    updates to ito channel

--- channels/ito.py
+++ channels/ito.py
@@ -27,8 +27,9 @@ ito = Channel('ito', modelfile, ito_params,
                                           'ito.G_to'])
 
 ### Exp 1 - IV curve
-iv_vsteps, iv_curr, _, _ = data.IV_Lu()
-iv_data = ExperimentData(x=iv_vsteps, y=iv_curr)
+iv_vsteps, iv_curr, iv_errs, _ = data.IV_Lu()
+iv_data = ExperimentData(x=iv_vsteps, y=iv_curr,
+                         errs=iv_errs)
 stim_times = [500, 30, 300, 500]
 stim_levels = [-80, -40, iv_vsteps, -80]
 def peak_curr(data):
@@ -42,8 +43,9 @@ iv_exp = Experiment(iv_prot, iv_data, lu_conditions)
 ito.add_experiment(iv_exp)
 
 ### Exp 2 - Activation time constant
-act_vsteps, act_tau, _, _ = data.ActTau_Xu()
-act_kin_data = ExperimentData(x=act_vsteps, y=act_tau)
+act_vsteps, act_tau, act_tau_errs, _ = data.ActTau_Xu()
+act_kin_data = ExperimentData(x=act_vsteps, y=act_tau,
+                              errs=act_tau_errs)
 stim_times = [500, 20, 4500, 500]
 stim_levels = [-70, -20, act_vsteps, -70]
 def fit_exp_rising_phase(data):
@@ -93,12 +95,12 @@ stim_times = [5000, 5000, 500]
 stim_levels = [inact_vsteps, 50, -70]
 def max_gto(data):
     return max(data[0]['ito.G_to'])
-def normalise_positives(sim_results):
+def normalise(sim_results, ind_var):
     m = max(sim_results, key=abs)
     sim_results = [result / m for result in sim_results]
     return sim_results
 inact_prot = ExperimentStimProtocol(stim_times, stim_levels,
                                     measure_index=1, measure_fn=max_gto,
-                                    post_fn=normalise_positives)
+                                    post_fn=normalise)
 inact_exp = Experiment(inact_prot, inact_data, xu_conditions)
 ito.add_experiment(inact_exp)
