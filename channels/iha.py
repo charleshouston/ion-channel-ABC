0c057c2154607d547d2fe1b84c0f68f5c6501d50
Charles Houston <charles.houston@protonmail.com>
2018-08-17 15:38:27 +0100

    Corrections to protocols for iha and new models

--- channels/iha.py
+++ channels/iha.py
@@ -6,19 +6,18 @@ import data.iha.data_iha as data
 import numpy as np
 
 
-modelfile = 'models/Majumder2016_iha.mmt'
+modelfile = 'models/Korhonen2009_iha.mmt'
 
-iha_params = {'k_yss1': (0, 100),
-              'k_yss2': (1, 10),
-              'k_ytau1': (0, 10),
-              'k_ytau2': (0, 1.0),
-              'k_ytau3': (0, 100),
-              'k_ytau4': (1, 100),
-              'k_ytau5': (0, 1.0),
-              'k_ytau6': (0, 100),
-              'k_ytau7': (0, 100),
+iha_params = {'p1': (0, 100),
+              'p2': (0, 100),
+              'p3': (0, 1),
+              'p4': (0, 100),
+              'p5': (0, 100),
+              'p6': (0, 1),
+              'p7': (0, 100),
+              'p8': (-100, 0),
               'k_i_haNa': (0, 1.0),
-              'g_ha': (0, 0.1)}
+              'g_ha': (0, 0.5)}
 
 iha = Channel('iha', modelfile, iha_params,
               vvar='membrane.V', logvars=['environment.time',
@@ -58,19 +57,21 @@ def fit_mono_exp(data):
     import scipy.optimize as so
     import warnings
     # Exclude initial rising phase of current
+    #curr = data[0]['iha.i_ha']
+    #curr_diff = np.diff(curr)
+    #index = 0
+    #if curr_diff[0] > 0:
+    #    turn_point = np.argwhere(curr_diff < 0)
+    #else:
+    #    turn_point = np.argwhere(curr_diff > 0)
+    #if turn_point.size > 0:
+    #    index = turn_point[0][0]+1
+    #else:
+    #    index = -1
+    #curr = curr[:index]
+    #time = data[0]['environment.time'][:index]
     curr = data[0]['iha.i_ha']
-    curr_diff = np.diff(curr)
-    index = 0
-    if curr_diff[0] > 0:
-        turn_point = np.argwhere(curr_diff < 0)
-    else:
-        turn_point = np.argwhere(curr_diff > 0)
-    if turn_point.size > 0:
-        index = turn_point[0][0]+1
-    else:
-        index = -1
-    curr = curr[:index]
-    time = data[0]['environment.time'][:index]
+    time = data[0]['environment.time']
     t0 = time[0]
     time = [(t - t0)/1000 for t in time] # Units in seconds.
 
@@ -78,13 +79,12 @@ def fit_mono_exp(data):
     with warnings.catch_warnings():
         try:
             def single_exp(t, I_max, I_diff, tau):
-                return I_max * (1 - np.exp(-t / tau)) - I_diff
-            [I_max, I_diff, tau], _ = so.curve_fit(
+                return I_max * np.exp(-t / tau) + I_diff
+            [I_max, _, tau], _ = so.curve_fit(
                     single_exp, time, curr, bounds=([-np.inf, -np.inf, 0],
                                                     np.inf))
-            I_amplitude = abs(I_diff - curr[0])
             np.seterr(**old_settings)
-            return [I_amplitude, tau]
+            return [I_max, tau]
         except:
             np.seterr(**old_settings)
             return [float("inf"), float("inf")]
