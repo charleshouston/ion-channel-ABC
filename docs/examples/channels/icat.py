e320e380f3739d7c3ea9ae1a669e183dffbdd1ad
Charles Houston <charles.houston11@imperial.ac.uk>
2018-11-08 17:25:04 +0000

    Explicitly including intracellular Ca2+ in icat setup

--- docs/examples/channels/icat.py
+++ docs/examples/channels/icat.py
@@ -28,6 +28,7 @@ iv_prot = ExperimentStimProtocol(stim_times, stim_levels,
                                  measure_index=1,
                                  measure_fn=max_icat)
 nguyen_conditions = dict(Ca_o=5000,
+                         Ca_subSL=0.2,
                          T=295)
 iv_exp = Experiment(iv_prot, iv_data, nguyen_conditions)
 
@@ -71,6 +72,7 @@ rec_prot = ExperimentStimProtocol(stim_times, stim_levels,
                                   measure_index=[1, 3], measure_fn=ratio_cond,
                                   post_fn=normalise)
 deng_conditions = dict(Ca_o=5000,
+                       Ca_subSL=0.2,
                        T=298)
 rec_exp = Experiment(rec_prot, rec_data, deng_conditions)
 
@@ -102,13 +104,9 @@ iv_vsteps2, _, _, _ = data.IV_Deng()
 iv_data2 = ExperimentData(x=iv_vsteps2, y=[0.,]*len(iv_vsteps2))
 stim_times = [5000, 300, 500]
 stim_levels = [-40, iv_vsteps2, -40]
-def max_icat(data):
-    return max(data[0]['icat.i_CaT'],
-               key=lambda x: abs(x))
 iv_prot2 = ExperimentStimProtocol(stim_times, stim_levels,
                                  measure_index=1,
                                  measure_fn=max_icat)
 iv_exp2 = Experiment(iv_prot2, iv_data2, deng_conditions)
-
 
 icat.add_experiments([iv_exp, act_exp, inact_exp, rec_exp, trace_exp, iv_exp2])
