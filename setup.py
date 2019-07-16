a5552594fabc4332badf0e17da7168f518a59160
Charles Houston <charles.houston@protonmail.com>
2019-07-16 15:33:32 +0100

    Would be fair to say a minor version bump required...

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='ionchannelABC',
-    version='0.1.2',
+    version='0.2.0',
     description='Approximate Bayesian computation for ion channel models',
     long_description=readme,
     author='Charles Houston',
