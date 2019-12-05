5f3ef5a9ae413693c22b69c7bf8bb0b135a994c7
Charles Houston <cph211@ic.ac.uk>
2019-12-05 13:17:22 +0000

    Updating whole cell calibration

--- docs/examples/hl1/experiments/whole_cell.py
+++ docs/examples/hl1/experiments/whole_cell.py
@@ -9,20 +9,20 @@ from paced whole cell simulation. 80pA/pF for 0.5ms at 1Hz for 100s.
 """
 
 # AP measurements
-mdp, mdp_sem, mdp_n = -67, 2, 25
+mdp, mdp_sem, mdp_n = -67, 2, 25 # maximum diastolic potential
 mdp_sd = np.sqrt(mdp_n)*mdp_sem
-dvdt_max, dvdt_max_sem, dvdt_max_n = 107, 7, 11
+dvdt_max, dvdt_max_sem, dvdt_max_n = 107, 7, 11 # maximum upstroke
 dvdt_max_sd = np.sqrt(dvdt_max_n)*dvdt_max_sem
-amp, amp_sem, amp_n = 105, 2, 11
+amp, amp_sem, amp_n = 105, 2, 11 # maximum amplitude of AP
 amp_sd = np.sqrt(amp_n)*amp_sem
-apd90, apd90_sem, apd90_n = 42, 9, 7
+apd90, apd90_sem, apd90_n = 42, 9, 7 # 90% repolarisation of AP
 apd90_sd = np.sqrt(apd90_n)*apd90_sem
 # CaT measurements
-t2p, t2p_sem, t2p_n = 59, 2, 6
+t2p, t2p_sem, t2p_n = 59, 2, 6 # CaT time to peak
 t2p_sd = np.sqrt(t2p_n)*t2p_sem
-CaTR50, CaTR50_sem, CaTR50_n = 157, 6, 6
+CaTR50, CaTR50_sem, CaTR50_n = 157, 6, 6 # CaT time to 50% repolarisation
 CaTR50_sd = np.sqrt(CaTR50_n)*CaTR50_sem
-CaTR90, CaTR90_sem, CaTR90_n = 397, 14, 6
+CaTR90, CaTR90_sem, CaTR90_n = 397, 14, 6 # CaT time to 90% repolarisation
 CaTR90_sd = np.sqrt(CaTR90_n)*CaTR90_sem
 ap_dataset = [np.asarray([[0], [mdp], [mdp_sd**2]]),
               np.asarray([[0], [dvdt_max], [dvdt_max_sd**2]]),
@@ -33,25 +33,13 @@ ap_dataset = [np.asarray([[0], [mdp], [mdp_sd**2]]),
               np.asarray([[0], [CaTR90], [CaTR90_sd**2]])]
 
 ap_protocol = myokit.pacing.blocktrain(
-    period=1000, duration=0.5, limit=101, offset=2
+    period=1000, duration=2, limit=101, offset=2
 )
 
 ap_conditions = {'extra.Ca_o': 1.8e3,
                  'extra.K_o' : 4.0e3,
                  'extra.Na_o': 130e3,
                  'phys.T'    : 295}
-
-# Ion channel pyABC runs
-pre_optimised = ['ina/hl1-ina.db',
-                 'ical/hl1-ical.db',
-                 'icat/hl1-icat.db',
-                 'iha/hl1-iha.db',
-                 'ik1/hl1-ik1.db',
-                 'ikr/hl1-ikr.db',
-                 'ito/hl1-ito.db',
-                 'ikss/hl1-ikss.db']
-pre_optimised = ['sqlite:////storage/hhecm/cellrotor/chouston/'+path
-                 for path in pre_optimised]
 
 def ap_sum_stats(data):
     output = []
@@ -71,7 +59,7 @@ def ap_sum_stats(data):
     try:
         decay = d.trim_left(t[peak_idx])['membrane.V']
         apd90_idx = np.argwhere(decay < np.max(v)-0.9*amp)[0][0]
-        apd90 = t[dvdt_max_idx+peak_idx+apd90_idx] - t[dvdt_max_idx]
+        apd90 = t[peak_idx+apd90_idx] - t[dvdt_max_idx]
     except:
         apd90 = float('inf')
     # CaT time to peak
@@ -79,18 +67,16 @@ def ap_sum_stats(data):
     cat_t2p = t[peak_cat_idx] - 2 # offset 2ms
     if cat_t2p < 0:
         cat_t2p = float('inf')
-    # CaT time to repolarisation 50%
+    # CaT time to repolarisation 50% and 90%
     peak_cat = np.max(CaT)
     try:
-        cat_r50_idx = np.argwhere(CaT < peak_cat - 0.5*CaT[0])[0][0]
-        cat_r50 = t[cat_r50_idx] - 2
+        decay = d.trim_left(t[peak_cat_idx])['calcium.Ca_i']
+        cat_r50_idx = np.argwhere(decay < peak_cat - 0.5*CaT[0])[0][0]
+        cat_r50 = t[peak_cat_idx+cat_r50_idx] - 2
+        cat_r90_idx = np.argwhere(decay < peak_cat - 0.9*CaT[0])[0][0]
+        cat_r90 = t[peak_cat_idx+cat_r90_idx] - 2
     except:
         cat_r50 = float('inf')
-    # CaT time to repolarisation 90%
-    try:
-        cat_r90_idx = np.argwhere(CaT < peak_cat - 0.9*CaT[0])[0][0]
-        cat_r90 = t[cat_r90_idx] - 2
-    except:
         cat_r90 = float('inf')
     return [mdp, dvdt_max, amp, apd90, cat_t2p, cat_r50, cat_r90]
 
